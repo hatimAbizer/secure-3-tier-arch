2d332f232577cb866aace13ab66c8a809d4fef5f
hatimAbizer <hatimabizer.tinwala@bcah.christuniversity.in>
2026-04-15 17:35:41 +0530

    Fix PM2 config to rely on dotenv in server.js

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -4,10 +4,8 @@ module.exports = {
     script: 'server.js',
     cwd: '/home/ec2-user/app',
     autorestart: true,
-    env_production: {
+    env: {
       NODE_ENV: 'production'
-    },
-    // Load .env file from app directory
-    env_file: '/home/ec2-user/app/.env'
+    }
   }]
 };
